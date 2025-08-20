209f9f231479f3f5931c8d20f8cd2e98107310b2
Azis JP <axzisalpha@gmail.com>
2025-08-20 06:16:16 +0000

    ketika di klik header, saya ingin ada animasi aosnya juga

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { Logo } from './logo';
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header data-aos="fade-down" className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
         <Logo />
         <div className="ml-auto flex items-center space-x-4">
