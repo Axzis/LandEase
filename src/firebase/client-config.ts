a34f78d232876f81e8c839d11570b0b86bfca2cc
Azis JP <axzisalpha@gmail.com>
2025-09-25 08:12:59 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDrWsPeG-OPyqkDc8di0zxLGXRd7xMW

--- src/firebase/client-config.ts
+++ src/firebase/client-config.ts
@@ -1,14 +1,14 @@
 'use client';
 
 // This file is designated as a client-only module.
-// It's safe to read environment variables here because it will only be
+// It's safe to use the provided configuration here as it will only be
 // executed in the browser.
 
 export const firebaseConfig = {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    };
+  apiKey: "AIzaSyDrWsPeG-OPyqkDc8di0zxLGXRd7xMW_6I",
+  authDomain: "landease-1g97g.firebaseapp.com",
+  projectId: "landease-1g97g",
+  storageBucket: "landease-1g97g.appspot.com",
+  messagingSenderId: "121206095671",
+  appId: "1:121206095671:web:4abd4a6ea473c72fb0e4b2"
+};
