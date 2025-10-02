d7c3e93136e122d5b52a79110d552a5e7dd2aca8
Azis JP <axzisalpha@gmail.com>
2025-10-02 01:13:47 +0000

    berikut firebase config saya const firebaseConfig = {   apiKey: "AIzaSyD

--- src/firebase/client-config.ts
+++ src/firebase/client-config.ts
@@ -8,7 +8,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDrWsPeG-OPyqkDc8di0zxLGXRd7xMW_6I",
   authDomain: "landease-1g97g.firebaseapp.com",
   projectId: "landease-1g97g",
-  storageBucket: "landease-1g97g.appspot.com",
+  storageBucket: "landease-1g97g.firebasestorage.app",
   messagingSenderId: "121206095671",
   appId: "1:121206095671:web:4abd4a6ea473c72fb0e4b2"
 };
