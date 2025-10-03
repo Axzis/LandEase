c76a4c7086b0f3011b339f9772310caf048e6f65
Azis JP <axzisalpha@gmail.com>
2025-10-03 06:15:05 +0000

    commit

--- src/app/api/imagekit/auth/route.ts
+++ src/app/api/imagekit/auth/route.ts
@@ -1,6 +1,15 @@
-import { imagekit } from "@/lib/imagekit-server"; // Impor instance yang sudah dibuat
+import ImageKit from "imagekit";
+
+// Tambahkan baris ini untuk memberitahu Next.js bahwa route ini dinamis
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
+  const imagekit = new ImageKit({
+    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+  });
+
   const result = imagekit.getAuthenticationParameters();
   return Response.json(result);
 }
